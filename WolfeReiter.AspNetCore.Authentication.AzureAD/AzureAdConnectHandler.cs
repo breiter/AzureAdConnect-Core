48c6212311f2bf202d0898a082bd77bf0f7e3d54
Brian Reiter <breiter@wolfereiter.com>
2018-03-05 11:04:51 +0200

    AzureAdHandler -> AzureAdConnectHandler.

--- WolfeReiter.AspNetCore.Authentication.AzureAD/AzureAdConnectHandler.cs
+++ WolfeReiter.AspNetCore.Authentication.AzureAD/AzureAdConnectHandler.cs
@@ -8,14 +8,21 @@ using Microsoft.Extensions.Options;
 
 namespace WolfeReiter.AspNetCore.Authentication.AzureAD
 {
-    public class AzureAdHandler : OpenIdConnectHandler, IAuthenticationSignOutHandler
+    /// <summary>
+    /// A per-request authentication handler for AzureAD that maps Groups to RoleClaims.
+    /// </summary>
+    public class AzureAdConnectHandler : OpenIdConnectHandler, IAuthenticationSignOutHandler
     {
-        public AzureAdHandler(IOptionsMonitor<OpenIdConnectOptions> options, ILoggerFactory logger, HtmlEncoder htmlEncoder, UrlEncoder encoder, ISystemClock clock)
+        public AzureAdConnectHandler(IOptionsMonitor<OpenIdConnectOptions> options, ILoggerFactory logger, HtmlEncoder htmlEncoder, UrlEncoder encoder, ISystemClock clock)
             : base(options, logger, htmlEncoder, encoder, clock)
         {
 
         }
 
+        /// <summary>
+        /// Invoked to process incoming OpenIdConnect messages.
+        /// </summary>
+        /// <returns>An <see cref="HandleRequestResult"/>.</returns>
         protected override async Task<HandleRequestResult> HandleRemoteAuthenticateAsync()
         {
             var baseResult = await base.HandleRemoteAuthenticateAsync();
